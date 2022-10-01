d1f2664c3c339d879a09e5f1a32cf0b6d505f2fc
Muhammadiqbalfattah <112533173+Muhammadiqbalfattah@users.noreply.github.com>
2022-10-01 09:48:50 +0700

    Add files via upload

---
+++ JavaOPP.java
@@ -0,0 +1,21 @@
+package SlidePPT;
+//create class
+
+public class JavaOPP {s
+    //create method
+    static void method(){
+
+    }
+    int x = 5;
+// create an object
+    public static void main(String[] args) {
+        JavaOPP obj = new JavaOPP();
+        JavaOPP obj2 = new JavaOPP();
+        //Modify Attributes
+        obj.x = 40;
+        obj2.x = 20;
+        System.out.println(obj.x);
+        System.out.println(obj2.x);
+
+    }
+}
